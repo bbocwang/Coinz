14e35dfb3d592a78e350954b98ee542499e3a3ab
stormwong <wonghao@tju.edu.cn>
2018-11-02 17:13:29 +0000

    added firebase

--- app/src/main/java/uk/ac/ed/coinz/LoginActivity.java
+++ app/src/main/java/uk/ac/ed/coinz/LoginActivity.java
@@ -1,13 +1,41 @@
 package uk.ac.ed.coinz;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    public void updateUI(FirebaseUser currentUser){
+
+    }
+    mAuth.createUserwithEmailAndPassword(email,password)
+            .addOnCompleteListener(this, task ->{
+        if (task.isSuccessful()) {
+            //sign in sucess,update ui with user info
+        } else {
+            //sign in failed, display a message to the user
+        }
+    });
+
+
     }
 }
