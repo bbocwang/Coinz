dc35074cd0801411933e3548eec98d051aac9665
stormwong <wonghao@tju.edu.cn>
2018-11-11 00:12:51 +0000

    created the downloading remote data chunk but not finished

--- app/src/main/java/uk/ac/ed/coinz/GameActivity.java
+++ app/src/main/java/uk/ac/ed/coinz/GameActivity.java
@@ -1,9 +1,14 @@
 package uk.ac.ed.coinz;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.common.util.IOUtils;
 import java.io.IOException;
@@ -11,6 +16,11 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
 import io.grpc.internal.IoUtils;
@@ -18,11 +28,114 @@ import io.grpc.internal.IoUtils;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class GameActivity extends AppCompatActivity {
+    private final String tag = "GameActivity";
+
+    Calendar calendar = Calendar.getInstance();
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+    private String currentDate = dateFormat.format(calendar.getTime());
+    private String downloadDate = "";//Format:YYYY/MM/DD
+    private final String preferencesFile = "LastDownloadDate";//for storing preferences
+    public String json = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        //download  the geojson from url
+        downloadjson();
+
+        TextView date = findViewById(R.id.Date);
+        date.setText(json);
+
+    }
+
+    private void downloadjson() {
+        DownloadFileTask downloadFileTask = new DownloadFileTask();
+        //http://homepages.inf.ed.ac.uk/stg/coinz/2018/09/28/coinzmap.geojson
+        if(currentDate == downloadDate) {
+            downloadDate = currentDate;
+        }
+            downloadFileTask.execute("http://homepages.inf.ed.ac.uk/stg/coinz/"
+                    + currentDate+"/coinzmap.geojson");
+
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        //restore preferences
+        SharedPreferences settings = getSharedPreferences(preferencesFile,
+                Context.MODE_PRIVATE);
+
+        //use""as default value
+        downloadDate = settings.getString("lastDownloadDate","");
+        Log.d(tag, "[onStart] Recalled lastDownloadDate is '"+downloadDate+"'");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(tag,"[onStop] Storing lastDownloadDate of "+downloadDate);
+        SharedPreferences settings = getSharedPreferences(preferencesFile,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("lastDownloadDate",downloadDate);
+        editor.apply();
+    }
+
+    void downloadComplete(String result){
+        json = result;
+
+    }
+
+
+
+
+
+
+
+    @SuppressLint("StaticFieldLeak")
+    public class DownloadFileTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadFileFromNetwork(urls[0]);
+            } catch (IOException e) {
+                return "Unable to load data. Please check your network connection";
+            }
+        }
+
+        private String loadFileFromNetwork(String urlString) throws IOException {
+            return readStream(downloadUrl(new URL(urlString)));
+        }
+
+        private InputStream downloadUrl(URL url) throws IOException {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            return conn.getInputStream();
+        }
+
+        @NonNull
+        private String readStream(InputStream stream) throws IOException {
+            Scanner s = new Scanner(stream).useDelimiter("\\A");
+            String result = s.hasNext() ? s.next() : "";
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            DownloadCompleteRunner.downloadComplete(result);
+        }
+
+
+
     }
 }
 
